d41f7b265a2b52f356bd2016eba1e007f4f94cdf
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-12-12 14:42:12 -0500

    Create randomWalk.java

---
+++ randomWalk.java
@@ -0,0 +1,21 @@
+public class randomWalk {
+    private int xCord;
+    private int yCord;
+    public randomWalk()
+    {
+        xCord = 150;
+        yCord = 150;
+        Rectangle box = new Rectangle(0, 0, 300, 300);
+        box.draw();
+        Ellipse border = new Ellipse(100, 100, 100, 100);
+        border.setColor(Color.RED);
+        border.fill(); 
+        Ellipse circle = new Ellipse(150, 150, 10, 10);
+        circle.setColor(Color.GREEN);
+        circle.fill();
+    }
+    public static void main(String args[])
+    {
+        randomWalk Test = new randomWalk();
+    }
+}
